fc52c0ac238c2d95f036ec8888c1e58ea3da8812
hiikitoss2 <104124820+hiikitoss2@users.noreply.github.com>
2022-04-21 10:47:36 +0300

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -25,7 +25,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://hikkibotik.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://hikkibotik2.herokuapp.com/' + TOKEN)
     return "!", 200
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
